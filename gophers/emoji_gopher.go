05292279ebb0d04323c2e0768f4da63fc7782117
Carolyn Van Slyck <me@carolynvanslyck.com>
2017-05-16 18:24:42 -0500

    emoji gopher solution

--- gophers/emoji_gopher.go
+++ gophers/emoji_gopher.go
@@ -1,14 +1,32 @@
 package gophers
 
+import "strings"
+
 // EmojiGopher replaces well-known words with their emoji equivalent.
 // For example, it replaces the word `love` with ❤️.
 type EmojiGopher struct{}
 
 func (g EmojiGopher) TransformMessage(msg string) string {
-	// TODO: Pick a set of words and replace them in the message with their emoji equivalent
+	// Solution 2 is more efficient
+	return g.solution2(msg)
+}
 
-	// Helpful links:
-	// * https://golang.org/pkg/strings/#Replace
+func (g EmojiGopher) solution1(msg string) string {
+	// Solution 1: Keep a mapping of replacements and apply them one at a time
+	// https://golang.org/pkg/strings/#Replace
+	emojis := map[string]string{
+		"poop": "💩",
+		"love": "💖",
+	}
+	for txt, emj := range emojis {
+		msg = strings.Replace(msg, txt, emj, -1)
+	}
+	return msg
+}
 
-	return "I ❤ emoji!"
+func (g EmojiGopher) solution2(msg string) string {
+	// Solution 2: Use a Replacer, takes a list of replacements and applies them in a single loop
+	// https://golang.org/pkg/strings/#Replacer
+	replacer := strings.NewReplacer("poop", "💩", "love", "💖")
+	return replacer.Replace(msg)
 }
